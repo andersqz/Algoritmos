d42c857b5fc2f201c262b4a9ed0729f14157aab9
Anderson Carvalho <anderson.carvalho@ufn.edu.br>
2025-08-05 18:49:55 -0300

    up2

---
+++ 01-casa/program2.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define TAM 5
+
+int main(){
+
+    int matriz[TAM][TAM];
+    int i, j;
+
+    for (i = 0; i < TAM; I++){
+        printf("matriz[i][i] =  ", i);
+        scanf("%d", &matriz[i][j]);
+    }
+
+    for (j = 0; j < TAM; j++){
+        printf("matriz[j][j] = %d%d", j);
+        scanf("%d", &matriz[i][j]);
+    }
+
+    return 0;
+}
