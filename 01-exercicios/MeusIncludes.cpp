b4f1088d98f6c59266476faa38d66d058cb7fa2d
Anderson Carvalho <anderson.carvalho@ufn.edu.br>
2025-08-12 21:52:42 +0000

    update

---
+++ 01-exercicios/MeusIncludes.cpp
@@ -0,0 +1,14 @@
+//segunda maneira de usar um struct
+typedef struct {
+    int valorGlicemia;
+    string data;
+    string hora;
+} Glicemia;
+ 
+ 
+typedef struct {
+    string nome;
+    string celular;
+    string data;
+    string descricao;
+} Emprestimo;
