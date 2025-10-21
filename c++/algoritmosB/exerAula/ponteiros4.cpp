20c6082e958aff9cad089ebd11c7e0cf39f278e7
Anderson Carvalho <anderson.carvalho@ufn.edu.br>
2025-10-21 00:06:59 +0000

    update files ponteiros

--- c++/algoritmosB/exerAula/ponteiros4.cpp
+++ c++/algoritmosB/exerAula/ponteiros4.cpp
@@ -1,3 +1,9 @@
+/*
+   faça um método que receba o vetor, 
+   substitua todos os numeros pares por zero e retorne a 
+   quantidade de números que foram substituidos. Contudo, não é permitido 
+   usar return
+   */
 #include <iostream>
 #include <ctime>
 #define TAM 5
@@ -16,12 +22,31 @@ void exibe(int *v, int n) {
     }
 }
 
+void substituiPares(int *v, int n, int *qtdTrocas)
+{
+    *qtdTrocas = 0;
+    for (int i = 0; i < n; i++) {
+        if (v[i] % 2 == 0) {
+            v[i] = 0;
+            (*qtdTrocas)++;
+        }
+    }
+}
 int main() {
    int vetor[TAM];
    popula(vetor, TAM);
+   
+   cout << "Vetor Original: ";
    exibe(vetor, TAM);
 
-   /*
-   faça um método que receba o vetor, substitua todos os numeros pares por zero e retorne a quantidade de números que foram substituidos. Contudo, não é permitido usar return
-   */
+   int substituidos;
+   substituiPares(vetor, TAM, &substituidos);
+
+   cout << "\nVetor apos substituir pares por zero: ";
+   exibe(vetor, TAM);
+
+   cout << "\nQtd de numeros substiuidos: " << substituidos << endl;
+   return 0;
+
+
 }
